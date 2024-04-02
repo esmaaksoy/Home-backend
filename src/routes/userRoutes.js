ec6cc68530aea70d1cba5727f7ef1a8eeba9f896
esma <esma.ea.aksoy@gmail.com>
2024-04-02 15:46:21 +0300

    feat: logout

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,10 +3,11 @@
 const router = require("express").Router();
 const User = require("../controllers/userController");
 const postCors = require("../middlewares/postCors");
+const getCors = require("../middlewares/getCors")
 
 // router.route("/login").post(postCors(), User.login);
 router.route("/login").all(postCors()).post(User.login);
 
-router.route("/logout").get(User.logout);
+router.route("/logout").get(getCors(),User.logout);
 
 module.exports = router;
